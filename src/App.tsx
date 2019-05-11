c366de52ef9a8468d9a6ba6a3b047cc738590ba8
John Huang <JohnjunHH@gmail.com>
2019-05-11 17:28:01 -0400

    More setup

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          HELLO WORLD
         </a>
       </header>
     </div>
