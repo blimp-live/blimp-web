f90bd767ba8664356afcfa732ac2ed251179fb57
John Huang <JohnjunHH@gmail.com>
2019-05-14 12:32:26 -0400

    Added material-ui library. Added an example of a button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 const App: React.FC = () => {
   return (
@@ -16,11 +17,22 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
+        <EnterButton></EnterButton>
           HELLO WORLD
         </a>
       </header>
     </div>
   );
 }
+
+const EnterButton = () => (
+  <div>
+    <Button type="submit" className="button" >
+      Test
+    </Button>
+    <br />
+    <br />
+  </div>
+);
 
 export default App;
