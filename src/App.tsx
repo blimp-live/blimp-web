a733ae9d88ccf66a740fd07f65e3f1ec31e1a514
Stanley Huang <stanleystanhuang@gmail.com>
2019-05-14 14:17:56 -0400

    Calculator exmaple setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Button from '@material-ui/core/Button';
-
-const App: React.FC = () => {
+import * as React from "react";
+import Calculator from './containers/calculator'
+export const App = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <EnterButton></EnterButton>
-          HELLO WORLD
-        </a>
-      </header>
+    <div>
+      <Calculator />
     </div>
   );
-}
-
-const EnterButton = () => (
-  <div>
-    <Button type="submit" className="button" >
-      Test
-    </Button>
-    <br />
-    <br />
-  </div>
-);
-
-export default App;
+};
+export default App
