10f0eb436cb585f53e2b1198f229bbe56e449bc8
Stanley Huang <stanleystanhuang@gmail.com>
2019-05-14 15:53:23 -0400

    Add routes for marketing, view, edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import * as React from "react";
-import { Route, Switch } from "react-router-dom";
+// Libraries
+import * as React from 'react'
+import { Route, Switch, Link } from 'react-router-dom'
+
+// Pages
 import Calculator from './containers/calculator'
+import { DashboardEdit } from './containers/dashboard-edit'
+import { DashboardView } from './containers/dashboard-view'
+import { MarketingPage } from './containers/marketing-page'
 
 export const App = (): JSX.Element => {
   return (
-    <div>
-      <Route exact={true} path="/" component={Calculator} />
-    </div>
-  );
-};
-export default App;
+    <React.Fragment>
+      <Route exact path="/" component={MarketingPage} />
+      <Route exact path="/calculator-example/" component={Calculator} />
+      <Route exact path="/:username/:dashboardName" component={DashboardView} />
+      <Route exact path="/:username/:dashboardName/edit" component={DashboardEdit} />
+    </React.Fragment>
+  )
+}
+export default App
---
+++ src/containers/marketing-page/index.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators, Dispatch } from "redux";
+import { RootState, rootReducer } from "../../reducers";
+import { Link } from "react-router-dom"
+
+interface Props {
+  actions: any;
+}
+
+export class MarketingPage extends React.Component<Props> {
+  render(){
+    return (
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/calculator-example/">Calculator Example</Link>
+            </li>
+            <li>
+              <Link to="/hackthenorth/2019/">Hack the North</Link>
+            </li>
+          </ul>
+        </nav>
+        <h1>Welcome Home!</h1>
+      </div>
+    );
+  }
+}
+
+const actions: any = Object.assign({}, null);
+
+function mapStateToProps(state: RootState) {
+  return {};
+}
+
+function mapDispatchToProps(dispatch: Dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  };
+}
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MarketingPage);
