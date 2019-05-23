2a981a8c1c5de751728672143472e074ee3efa14
John Huang <JohnjunHH@gmail.com>
2019-05-23 17:09:41 -0400

    Added logic to drag and drop into different sections

--- src/App.tsx
+++ src/App.tsx
@@ -22,38 +22,71 @@ class App extends React.Component {
     }
 
     // @ts-ignore
-    const section = this.state.sections[source.droppableId];
-    const newWidgetIds = Array.from(section.widgetIds);
-    newWidgetIds.splice(source.index, 1);
-    newWidgetIds.splice(destination.index, 0, draggableId);
+    const startSection = this.state.sections[source.droppableId];
+    // @ts-ignore
+    const finishSection = this.state.sections[destination.droppableId];
 
-    const newSection = {
-      ...section,
-      widgetIds: newWidgetIds,
+    if (startSection === finishSection){
+      const newWidgetIds = Array.from(startSection.widgetIds);
+      newWidgetIds.splice(source.index, 1);
+      newWidgetIds.splice(destination.index, 0, draggableId);
+
+      const newSection = {
+        ...startSection,
+        widgetIds: newWidgetIds,
+      };
+
+      const newState = {
+        ...this.state,
+        sections: {
+          ...this.state.sections,
+          [newSection.id]: newSection,
+        },
+      };
+
+      this.setState(newState);
+      return;
+    }
+
+    const startWidgetIds = Array.from(startSection.widgetIds);
+    startWidgetIds.splice(source.index, 1);
+    const newStartWidgetIds = {
+      ...startSection,
+      widgetIds: startWidgetIds,
+    };
+
+    const finishWidgetIds = Array.from(finishSection.widgetIds);
+    finishWidgetIds.splice(destination.index, 0, draggableId);
+    const newFinishWidgetIds = {
+      ...finishSection,
+      widgetIds: finishWidgetIds,
     };
 
     const newState = {
-      ...this.state,
-      sections: {
-        ...this.state.sections,
-        [newSection.id]: newSection,
-      },
-    };
-
+        ...this.state,
+        sections: {
+          ...this.state.sections,
+          [newStartWidgetIds.id]: newStartWidgetIds,
+          [newFinishWidgetIds.id]: newFinishWidgetIds,
+        },
+      };
     this.setState(newState);
+    
   }
 
   render() {
     return (
       <DragDropContext onDragEnd={this.onDragEnd}>
-        {this.state.sectionOrder.map(sectionId => {
-        // @ts-ignore
-        const section = this.state.sections[sectionId];
-        // @ts-ignore
-        const widgets = section.widgetIds.map(widgetId => this.state.widgets[widgetId]);
-        // @ts-ignore
-        return <Section key={section.id} section={section} widgets={widgets} />;
-      })}
+        <div className="container">
+          {this.state.sectionOrder.map(sectionId => {
+            // @ts-ignore
+            const section = this.state.sections[sectionId];
+            // @ts-ignore
+            const widgets = section.widgetIds.map(widgetId => this.state.widgets[widgetId]);
+            // @ts-ignore
+            return <Section key={section.id} section={section} widgets={widgets} />;
+          })}
+        </div>
       </DragDropContext>
     );
   }
