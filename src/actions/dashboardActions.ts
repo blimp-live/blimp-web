ba4be294cf70e158fb9848085183aec799c414e8
Stanley Huang <stanleystanhuang@gmail.com>
2019-06-04 11:57:59 -0400

    Add load dashboard (#9)

--- src/actions/dashboardActions.ts
+++ src/actions/dashboardActions.ts
@@ -30,7 +30,26 @@ export function setDashboard(contents: DashboardModel) {
   }
 }
 
-export function saveDashboard(name: String, userId: number, contents: DashboardModel) {
+export function loadDashboard(id: number) {
+  return (dispatch: any) => {
+    dispatch(fetchingDashboard());
+
+    fetch(`${root}/dashboard/id/${id}`, {
+      method: 'GET'
+    }).then(
+        (response: any) => response.json()
+    ).then(
+        (contents: any) => dispatch(setDashboard(contents))
+    ).catch(
+        (err: any) => {
+          console.error(`NETWORK ERROR: ${err.message}`)
+          dispatch(setDashboard(sampleDashboard))
+        }
+    )
+  }
+}
+
+export function saveDashboard(id: number, userId: number, contents: DashboardModel) {
   // This is making a post request to the api?
 
 }
