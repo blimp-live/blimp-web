82a06b251c3049531b7ee9311a665f60159d6c21
heoegema <hollyoegema@gmail.com>
2019-06-04 12:11:20 -0400

    Adding View State Reducer and actions

--- src/reducers/userReducer.ts
+++ src/reducers/userReducer.ts
@@ -1,10 +1,6 @@
 import { UserModel } from '../interfaces/userModel';
 
-<<<<<<< HEAD
-const initialState: userModel = {
-=======
 const initialState: UserModel = {
->>>>>>> d2b7aeef7e4557e08ed3256ceed745283b588bfa
   imgUrl: '',
   email: '',
   name: '',
@@ -15,9 +11,8 @@ export function userReducer(
   state = initialState,
   action: any
 ): UserModel {
-
   // TODO Fill in once we figure out how we're doing
   // authentication
   // We might want to use Passport.js
-  return initialState;
+  return state
 }
