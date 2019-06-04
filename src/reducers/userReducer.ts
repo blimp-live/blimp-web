e7dab4e9a191b33c3497c3f1a125dbc24834514b
Holly Oegema <hollyoegema@gmail.com>
2019-06-04 13:00:03 -0400

    Adding View State Reducer  (#12)
    * Addings skeletons for reducers and actions
    
    * Adding View State Reducer and actions
    
    * Adding View State Reducer and actions
    
    * Remove garbage

--- src/reducers/userReducer.ts
+++ src/reducers/userReducer.ts
@@ -11,8 +11,8 @@ export function userReducer(
   state = initialState,
   action: any
 ): UserModel {
-  return initialState;
   // TODO Fill in once we figure out how we're doing
   // authentication
   // We might want to use Passport.js
+  return state
 }
--- src/reducers/viewReducer.ts
+++ src/reducers/viewReducer.ts
@@ -1,8 +1,9 @@
 import { ViewStateModel } from '../interfaces/viewStateModel';
 
 import {
-
-
+  EDIT_DASHBOARD,
+  VIEW_DASHBOARD,
+  VIEW_HOME
 } from '../actions/viewStateActions';
 
 const initialState: ViewStateModel = {
@@ -15,5 +16,23 @@ export function viewStateReducer(
   state = initialState,
   action: any
 ): ViewStateModel {
-  return initialState;
+  switch(action.type) {
+    case VIEW_HOME:
+      return {
+        ...state,
+        home: true,
+      }
+    case VIEW_DASHBOARD:
+      return {
+        ...state,
+        viewDashboard: true,
+      }
+    case EDIT_DASHBOARD:
+      return {
+        ...state,
+        editDashboard: true,
+      }
+    default:
+      return state
+  }
 }
