7e5e7e72c922c95741e4cc9dde78cf36f174faf8
John Huang <JohnjunHH@gmail.com>
2019-06-27 15:11:22 -0400

    Removed overflow-y which caused a bug in the UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
+import './index.css';
 import { Provider } from "react-redux";
 import { configureStore, history } from "./reducers/store";
 import { ConnectedRouter } from "react-router-redux";
