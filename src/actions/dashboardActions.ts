b9fbc475ecd01e8bafd0c14d153799bc36d02dfa
heoegema <hollyoegema@gmail.com>
2019-07-16 14:03:00 -0400

    Adding calls for Add widget and remove widget things

--- src/actions/dashboardActions.ts
+++ src/actions/dashboardActions.ts
@@ -97,14 +97,6 @@ export function removeWidget(widgetId: String) {
   }
 }
 
-export function addWidget(widgetId: String) {
-  // This will add a Widget to the current contents
-}
-
-export function removeWidget(widgetId: String) {
-
-}
-
 export function createDashboard(name: String, userId: number) {
   return (dispatch: any) => {
     dispatch(fetchingDashboard());
