23f4ddf89a1e3e1acfc6e069611337909e2c1ebc
Stanley Huang <stanleystanhuang@gmail.com>
2019-07-18 14:54:02 -0400

    Add in Chai support

--- src/tests/dashboard-models-utils-test.ts
+++ src/tests/dashboard-models-utils-test.ts
@@ -1,11 +1,11 @@
 import mocha from 'mocha';
-import {assert} from 'chai';
+let chai = require('chai');
 
 describe('Hello function', () => {
 
   it('should return hello world', () => {
     const result = "Hello world!";
-    assert(result, 'Hello world!');
+    chai.assert(result, 'Hello world!');
   });
 
 });
