5ceab58f1a394701de8658947550d3fb2cb57ccb
Stanley Huang <stanleystanhuang@gmail.com>
2019-07-23 12:58:18 -0400

    Add Sectioning Logic and Sample Hack the North layout (#28)
    * WIP
    
    * Add HTN state
    
    * Add sectioning logic and fix up htn state

--- src/components/dashboard/section/index.tsx
+++ src/components/dashboard/section/index.tsx
@@ -9,9 +9,32 @@ interface Props {
 
 export class Section extends React.Component<Props> {
   render() {
+    const children = this.props.children
+    const relativeSize = this.props.relativeSize
     return (
       <div className={`${styles.section} ${this.props.sectionDivision === 'HORIZONTAL' ? styles.sectionHorizontal : styles.sectionVertical} `}>
-        {this.props.children}
+        {React.Children.map(children, (child, i) => {
+          // Generate a random colour
+          let colour = [];
+          for (var j = 0; j < 3; j++) {
+            colour.push(Math.floor(Math.random()*256));
+          }
+          return (
+            <div style={{
+              backgroundColor: `rgb(${colour[0]}, ${colour[1]}, ${colour[2]})`,
+              // Sizing logic
+              flexBasis: `${relativeSize[i]*100}%`,
+              // When the section is divided horizontally
+              // height is constrained, but width takes up 100% of available space
+              // vice versa for the vertical case
+              width: this.props.sectionDivision == 'HORIZONTAL' ? '100%' : `${relativeSize[i]*100}%`,
+              height: this.props.sectionDivision == 'VERTICAL' ? '100%' : `${relativeSize[i]*100}%`,
+              overflow: 'hidden',
+            }}>
+              {child}
+            </div>
+          )
+        })}
       </div>
     );
   }
--- src/reducers/dashboardReducer.ts
+++ src/reducers/dashboardReducer.ts
@@ -8,30 +8,116 @@ import {
 } from '../actions/dashboardActions';
 
 const Root : RootNodeModel = {
+  type: 'RootNode',
   children: [
     {
-      children: [
-        {
-          widgetType: 'Clock',
-          id: uuid4(),
-          options: null,
-          version: 1.0,
-          type: 'WidgetModel',
-        } as WidgetModel,
-        {
-          widgetType: 'Clock',
-          id: uuid4(),
-          options: null,
-          version: 1.0,
-          type: 'WidgetModel',
-        } as WidgetModel,
-      ],
       sectionDivision: 'VERTICAL',
       relativeSize: [0.5, 0.5],
       type: 'SectionModel',
+      children: [
+        {
+          // Left side of dashboard
+          sectionDivision: 'HORIZONTAL',
+          relativeSize: [0.5, 0.5],
+          type: 'SectionModel',
+          children: [
+            {
+              // Live Feed
+              widgetType: 'IFrameComponent',
+              options: null,
+              version: 1.0,
+              type: 'WidgetModel',
+            } as WidgetModel,
+            {
+              // Schedule
+              widgetType: 'ScrollingText',
+              options: null,
+              version: 1.0,
+              type: 'WidgetModel',
+            } as WidgetModel,
+          ],
+        } as SectionNodeModel,
+        {
+          // Right side of dashboard
+          sectionDivision: 'HORIZONTAL',
+          relativeSize: [0.20, 0.30, 0.4, 0.10],
+          type: 'SectionModel',
+          children: [
+            {
+              // Top infobar
+              sectionDivision: 'VERTICAL',
+              relativeSize: [0.5, 0.5],
+              type: 'SectionModel',
+              children: [
+                {
+                  // Top row of top infobar
+                  sectionDivision: 'HORIZONTAL',
+                  relativeSize: [0.5, 0.5],
+                  type: 'SectionModel',
+                  children: [
+                    {
+                      // Hack the North Logo
+                      widgetType: 'HelloWorld',
+                      options: null,
+                      version: 1.0,
+                      type: 'WidgetModel',
+                    } as WidgetModel,
+                    {
+                      // Time
+                      widgetType: 'Clock',
+                      options: null,
+                      version: 1.0,
+                      type: 'WidgetModel',
+                    } as WidgetModel,
+                  ],
+                } as SectionNodeModel,
+                {
+                  // Bottom row of top infobar
+                  sectionDivision: 'HORIZONTAL',
+                  relativeSize: [0.5, 0.5],
+                  type: 'SectionModel',
+                  children: [
+                    {
+                      // Weather
+                      widgetType: 'ExampleComponent',
+                      options: null,
+                      version: 1.0,
+                      type: 'WidgetModel',
+                    } as WidgetModel,
+                    {
+                      // Countdown
+                      widgetType: 'Countdown',
+                      options: null,
+                      version: 1.0,
+                      type: 'WidgetModel',
+                    } as WidgetModel,
+                  ],
+                } as SectionNodeModel,
+              ],
+            } as SectionNodeModel,
+            {
+              widgetType: 'ExampleComponent',
+              options: null,
+              version: 1.0,
+              type: 'WidgetModel',
+            } as WidgetModel,
+            {
+              widgetType: 'HelloWorld',
+              options: null,
+              version: 1.0,
+              type: 'WidgetModel',
+            } as WidgetModel,
+            {
+              widgetType: 'ScrollingText',
+              options: null,
+              version: 1.0,
+              type: 'WidgetModel',
+            } as WidgetModel,
+          ],
+        } as SectionNodeModel,
+      ],
     } as SectionNodeModel,
    ],
-  type: 'RootNode',
 }
 
 const initialState: DashboardModel = {
