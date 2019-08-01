a8bd96d0a3c0083fcefd9bd9e2041dca9999ed98
heoegema <hollyoegema@gmail.com>
2019-08-01 15:29:48 -0400

    Resolve conflicts

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -3,18 +3,15 @@ import Button from '@material-ui/core/Button';
 import styles from "./dashboard.module.css"
 import { DashboardItem } from "./dashboardItem";
 import { DashboardContentsModel } from "../../interfaces/dashboardModel";
-<<<<<<< HEAD
 import { DragDropContext } from 'react-beautiful-dnd';
 import { connect } from "react-redux";
 import { RootState } from "../../reducers";
 import { Dispatch, bindActionCreators } from "redux";
-import * as dashboardActions from "../../actions/dashboardActions";
-=======
 import { Drawer } from '@material-ui/core';
 import { WidgetList } from '../../containers/widget-list';
 import IconButton from '@material-ui/core/IconButton';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
->>>>>>> Starting files
+import * as dashboardActions from "../../actions/dashboardActions";
 
 interface Props {
   contents: DashboardContentsModel;
@@ -22,7 +19,6 @@ interface Props {
   actions: any;
 }
 
-<<<<<<< HEAD
 export class Dashboard extends React.Component<Props> {
   onDragEnd = result => {
     // Sample result format
@@ -47,45 +43,6 @@ export class Dashboard extends React.Component<Props> {
     // If no destination
     if (!destination) {
       return;
-=======
-interface State {
-    widget_list_open: boolean,
-}
-
-export class Dashboard extends React.Component<Props, State> {
-    constructor(props) {
-      super(props);
-      this.toggleWidgetList = this.toggleWidgetList.bind(this);
-
-      this.state = {
-        widget_list_open: true,
-      };
-    }
-
-    toggleWidgetList() {
-      this.setState({ widget_list_open: !this.state.widget_list_open});
-    }
-
-    render(){
-        const rootNode = this.props.contents.sections[this.props.contents.rootSection] || this.props.contents.widgets[this.props.contents.rootSection]
-        return (
-          <div>
-            <div className={styles.dashboard}>
-                <DashboardItem node={rootNode} contents={this.props.contents} widgets={this.props.widgets} />
-            </div>
-            <Drawer
-             anchor="right"
-             variant="persistent"
-             open={this.state.widget_list_open}
-             onClose={this.toggleWidgetList}>
-              <IconButton onClick={this.toggleWidgetList}>
-              <ChevronRightIcon/>
-             </IconButton>
-              <WidgetList actions={false}/>
-            </Drawer>
-          </div>
-        );
->>>>>>> Starting files
     }
 
     // If dropped in the same place
